6bc2503b92ea470be2ebca9168ee2d78b866157b
appsdeveloper <appsdeveloper22@gmail.com>
2022-05-02 10:13:08 +0530

    "api"

--- app/src/main/java/com/example/patternclinic/home/drawerFragments/messages/ChatActivity.kt
+++ app/src/main/java/com/example/patternclinic/home/drawerFragments/messages/ChatActivity.kt
@@ -1,10 +1,15 @@
 package com.example.patternclinic.home.drawerFragments.messages
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.patternclinic.R
 import com.example.patternclinic.databinding.ActivityChatBinding
+import com.example.patternclinic.utils.showToast
+import microsoft.aspnet.signalr.client.Credentials
+import microsoft.aspnet.signalr.client.Platform
+import microsoft.aspnet.signalr.client.http.android.AndroidPlatformComponent
+import microsoft.aspnet.signalr.client.hubs.HubConnection
 
 class ChatActivity : AppCompatActivity() {
     lateinit var binding: ActivityChatBinding
@@ -14,5 +19,17 @@ class ChatActivity : AppCompatActivity() {
         binding.ivBack.setOnClickListener {
             finish()
         }
+        Platform.loadPlatformComponent(AndroidPlatformComponent())
+        var credential=Credentials(){
+            it.addHeader("username", "username"); //get username
+        }
+        var url="http://patternclinicapis.harishparas.com/ChatHub"
+        var hubConnection=HubConnection(url)
+        hubConnection.credentials=credential
+        hubConnection.connected {
+            showToast("connected")
+        }
+
+
     }
 }
